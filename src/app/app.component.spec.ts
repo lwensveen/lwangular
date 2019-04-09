dae5c312938e14586f790baa78fa37a27191fff2
Lodewijk Wensveen <lodewijkwensveen@gmail.com>
2019-04-09 15:23:59 +0200

    Renamed project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lodewijkwensveen'`, () => {
+  it(`should have as title 'lwangular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lodewijkwensveen');
+    expect(app.title).toEqual('lwangular');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lodewijkwensveen!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lwangular!');
   });
 });
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/lodewijkwensveen'),
+      dir: require('path').join(__dirname, '../coverage/lwangular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
