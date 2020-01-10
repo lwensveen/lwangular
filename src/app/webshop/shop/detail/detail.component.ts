3906f8c392b042a69972aa87981df9202e51771e
Lodewijk Wensveen <lodewijkwensveen@gmail.com>
2020-01-10 17:50:09 +0100

    Added href to shoppingcart and added title routeparam to getphonebyid

--- src/app/webshop/shop/detail/detail.component.ts
+++ src/app/webshop/shop/detail/detail.component.ts
@@ -10,21 +10,26 @@ import { FormBuilder, FormGroup } from '@angular/forms';
     styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
+    id: string;
     myForm: FormGroup;
     phone: Phone;
     step = 0;
+    title: string;
 
     constructor(
         private $phonesService: PhonesService,
         private fb: FormBuilder,
-        private route: ActivatedRoute,
+        private activatedRoute: ActivatedRoute,
     ) {
     }
 
     ngOnInit() {
-        const id = +this.route.snapshot.paramMap.get('id');
+        this.activatedRoute.paramMap.subscribe(params => {
+            this.id = params.get('id');
+            this.title = params.get('title');
+        });
 
-        this.$phonesService.getPhonebyId(id).subscribe(phone => this.phone = phone[0]);
+        this.$phonesService.getPhonebyId(this.title, this.id).subscribe(phone => this.phone = phone[0]);
 
         this.myForm = this.fb.group({
             contractDuration: ['1'],
