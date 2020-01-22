93672219cf676ee114565f04989522c905b43fd8
Lodewijk Wensveen <lodewijkwensveen@gmail.com>
2020-01-22 15:22:18 +0100

    formatting fix

--- src/app/csv-upload/csv-upload.component.ts
+++ src/app/csv-upload/csv-upload.component.ts
@@ -10,7 +10,6 @@ import { CsvService } from './csv.service';
     styleUrls: ['./csv-upload.component.scss']
 })
 export class CsvUploadComponent implements OnInit, OnDestroy {
-
     public title = 'CSV import';
     public $csv = new Subject<Person[]>();
     public $handleCSV = new Subject<any>();
@@ -36,36 +35,38 @@ export class CsvUploadComponent implements OnInit, OnDestroy {
 
     ngOnInit(): void {
 
-        this.subscriptions.push(this.$handleCSV.pipe(
-            distinctUntilChanged(),
-        ).subscribe(evt => {
-                const files = evt.target.files;
-                const file = files[0];
-                const reader = new FileReader();
-                reader.readAsText(file);
+        this.subscriptions.push(
+            this.$handleCSV.pipe(
+                distinctUntilChanged(),
+            ).subscribe(evt => {
+                    const files = evt.target.files;
+                    const file = files[0];
+                    const reader = new FileReader();
+                    reader.readAsText(file);
 
-                reader.onload = () => {
-                    const csv = reader.result;
-                    this.csvData = this.csvService.extractData(csv).data;
-                    this.persons = this.csvData.persons;
-                    this.headers = this.csvData.headers;
+                    reader.onload = () => {
+                        const csv = reader.result;
+                        this.csvData = this.csvService.extractData(csv).data;
+                        this.persons = this.csvData.persons;
+                        this.headers = this.csvData.headers;
 
-                    this.$csv.next(this.persons);
-                };
-            }
-        ));
+                        this.$csv.next(this.persons);
+                    };
+                }
+            ));
 
-        this.subscriptions.push(this.$handleIssueCountFilter.pipe(
-            distinctUntilChanged(),
-        ).subscribe(evt => {
+        this.subscriptions.push(
+            this.$handleIssueCountFilter.pipe(
+                distinctUntilChanged(),
+            ).subscribe(evt => {
 
-            if (evt.data) {
-                this.filterBy(parseInt(evt.data, 10));
-                return;
-            }
+                if (evt.data) {
+                    this.filterBy(parseInt(evt.data, 10));
+                    return;
+                }
 
-            this.$csv.next(this.persons);
-        }));
+                this.$csv.next(this.persons);
+            }));
 
     }
 
